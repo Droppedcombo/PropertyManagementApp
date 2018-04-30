3f530ca8cabcface96a21c3799ff85a1708dd7a1
benrojas <droppedcombo7@gmail.com>
2018-04-30 15:25:46 -0600

    saving auth token to local storage

---
+++ src/components/newsletter/newsletter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Newsletter extends Component {
+    render() {
+        return (
+            <div>
+                newsletter componenet
+            </div>    
+        )
+    }
+} 
+
+export default Newsletter;
