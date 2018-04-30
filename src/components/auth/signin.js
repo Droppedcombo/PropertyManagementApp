21128c81312dae3d42dd956aae7ec319b3c1fddb
benrojas <droppedcombo7@gmail.com>
2018-04-30 14:36:33 -0600

    Set additional signin functions

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import * as actions from '../../actions';
+import { connect } from 'react-redux';
+
 
 class Signin extends Component {
-    render(){
-        return (
-            <div>
-                sign in form
-            </div>
+
+    renderInput(field) {
+        return <input className="form-control" {...field.input} type="field.type" />
+    }
+
+    handleFormSubmit({email, password}) {
+        this.props.signinUser({email, password})
+    }
+
+    render() {
+        const { handleSubmit } = this.props;
+        return  (
+            <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <label htmlFor="email">Email</label>
+                <Field name="email" component={this.renderInput} type="text"/>
+                <label htmlFor="password">Password</label>
+                <Field name="password" component={this.renderInput} type="password"/>
+
+                <button action="submit" className="btn btn-primary">Sign In</button>
+            </form>
         )
     }
 }
 
-export default Signin;
+function mapStateToProps(state) {
+    return { state }
+}
+
+
+Signin = reduxForm({form: "siginin"})(Signin)
+export default connect(mapStateToProps, actions)(Signin); 
